c7e569d6f86920e46c4f2db9050001b2e880396d
prajwalbagewadi <bagewadi.prajwal44@gmail.com>
2025-02-27 11:28:12 +0530

    first commit

---
+++ 00_welcome/index.php
@@ -0,0 +1,20 @@
+<?php //open 
+
+//echo : used to display string/text output on webpage.
+echo "helloworld 00 php";
+echo "
+
+<html>
+    <head>
+        <title>Welcome page.</title>
+    </head>
+    <body>
+        <h1>welcome to php.</h1>
+        <h1>helloworld php.</h1>
+        <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, aliquam.</h1>
+    </body>
+</html>
+
+";
+
+?> //close
